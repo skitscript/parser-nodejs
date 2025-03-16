783a77b47c3e4028fc557746d6c83355c65ba1b9
siliconspecter <siliconspecter@outlook.com>
2025-03-16 23:12:37 +0000

    Fix out-of-spec case changes.

--- internals/normalizeIdentifier/index.ts
+++ internals/normalizeIdentifier/index.ts
@@ -16,10 +16,37 @@ export const normalizeIdentifier = (
   const identifier = {
     verbatim,
     normalized: verbatim
-      .toLowerCase()
       .replace(identifierFilteredCharacterRegex, ' ')
       .trim()
-      .replace(/\s+/g, '-'),
+      .replace(/\s+/g, '-')
+
+      // TODO: These will be removed once this is fed from the lower case accumulator.
+      .replace(/A/g, 'a')
+      .replace(/B/g, 'b')
+      .replace(/C/g, 'c')
+      .replace(/D/g, 'd')
+      .replace(/E/g, 'e')
+      .replace(/F/g, 'f')
+      .replace(/G/g, 'g')
+      .replace(/H/g, 'h')
+      .replace(/I/g, 'i')
+      .replace(/J/g, 'j')
+      .replace(/K/g, 'k')
+      .replace(/L/g, 'l')
+      .replace(/M/g, 'm')
+      .replace(/N/g, 'n')
+      .replace(/O/g, 'o')
+      .replace(/P/g, 'p')
+      .replace(/Q/g, 'q')
+      .replace(/R/g, 'r')
+      .replace(/S/g, 's')
+      .replace(/T/g, 't')
+      .replace(/U/g, 'u')
+      .replace(/V/g, 'v')
+      .replace(/W/g, 'w')
+      .replace(/X/g, 'x')
+      .replace(/Y/g, 'y')
+      .replace(/Z/g, 'z'),
     fromColumn,
     toColumn: fromColumn + verbatim.length - 1
   }
